b0d4d6023a4d74fef9cb81a0af6fec762a3239c9
sgellio4 <sgellio4@ncsu.edu>
2024-01-14 19:35:56 -0500

    Modified Naming Convention, Added Comparison Evaluator, Added gitignore

---
+++ Project_Euler_Problem_003.py
@@ -0,0 +1,24 @@
+import time
+from HelperFunctions import get_prime_factors
+
+# Pretty trivial problem overall
+def compute():
+    working_num = 600851475143
+    iterating_factor = 2
+    
+    while (iterating_factor < working_num):
+        if (working_num % iterating_factor == 0):
+            working_num /= iterating_factor
+        else:
+            iterating_factor += 1
+    return str(iterating_factor)
+
+
+if __name__ == "__main__":
+    starttime = time.time()
+    for i in range(10000):
+        compute()
+    elapsedtime = time.time() - starttime
+    print("Took", str(int(round(elapsedtime * 1000))), "ms")
+
+    
