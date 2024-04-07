f2970da4fa14a94ad72c0efb5740234accf47e35
sellio5263 <sgelliott27@gmail.com>
2024-04-07 14:42:09 -0400

    Comment and fix Problem 35

--- SolutionComparison.py
+++ SolutionComparison.py
@@ -1,15 +1,16 @@
-import time
-from Solutions.Project_Euler_Problem_032 import compute, compute_2
+import sys, time
+sys.path.append("C:\\Users\\scott\\OneDrive\\Documents\\ProjectEuler\\Solutions")
+from Solutions.Project_Euler_Problem_035 import compute, compute_2
 
 if __name__ == "__main__":
     starttime = time.time()
-    for i in range(1000):
+    for i in range(10):
         compute()
     elapsedtime = time.time() - starttime
     print("Compute Took", str(int(round(elapsedtime * 1000))), "ms")
 
     starttime = time.time()
-    for i in range(1000):
+    for i in range(10):
         compute_2()
     elapsedtime = time.time() - starttime
     print("Compute_2 Took", str(int(round(elapsedtime * 1000))), "ms")
